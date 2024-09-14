d40780c7bc3e63b4b8e02fc418d230129c722764
SunbirG <sunbirghag@gmail.com>
2024-09-13 17:28:59 -0700

    Testing stylesheet
    Testing style sheet injection from github url

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.title = 'Script.js is working';
+const link = document.createElement('link');
+
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'https://github.com/SunbirG/WfTakeHome/blob/5bffa842d8537de01591879e4cf317e0c0608cdd/style.css';
+
+document.head.appendChild(link);
